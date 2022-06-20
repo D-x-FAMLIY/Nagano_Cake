f77dfbcacf897f0042f357cc0a5f67d2ac9799ce
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-20 06:31:30 +0000

    cart_item編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,13 @@
 class Public::CartItemsController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
+    @cart_items = CartItem.all
   end
 
   def create
+    
     @product = Product.find(cart_item_params[:product_id])
+    @cart_item.save
   end
 
   private

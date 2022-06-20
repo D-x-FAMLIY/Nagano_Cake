57f21e1833046036880c672a6dacc24b9ef925ad
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-20 05:39:46 +0000

    [Add]cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,14 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @product = Product.find(params[:product_id])
+  end
+
+  def create
+    @product = Product.find(cart_item_params[:product_id])
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:product_id, :quantity)
   end
 end

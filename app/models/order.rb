1f43247491d2b832a168f7b70516ae4e35cb8b02
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-06-21 06:02:28 +0000

    order追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
   has_many :product_orders, dependent: :destroy
   
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { no_payment: 0, confirm_payment: 1, production: 2, ready_to_ship: 3, sent: 4 }
 
   validates :name, presence:true
   validates :post_code, presence:true

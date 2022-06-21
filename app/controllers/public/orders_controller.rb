fb154618b0535c3e8c63f9a9f4b18faf1d586d5f
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-06-21 13:23:59 +0000

    レイアウト、orders編集

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,7 +6,7 @@ class Public::OrdersController < ApplicationController
   
   def confirm
     @order = Order.new(order_params)
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items
     @order.post_code = current_customer.post_code
     @order.address = current_customer.address
     @order.name = current_customer.first_name + current_customer.last_name
@@ -18,6 +18,7 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     if @order.save
       redirect_to public_orders_complete_path
+      current_customer.cart_items.destroy_all
     else
       render :new
     end

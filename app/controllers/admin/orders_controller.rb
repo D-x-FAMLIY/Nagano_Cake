a9e7ade7b25c15795242f38a61012b10956b60c7
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-22 06:50:58 +0000

    修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,19 +1,20 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @product_orders = @order.product_orders
   end
 
   def top
     # ページネーション記述(仮)
     @orders = Order.page(params[:page]).per(10)
   end
-  
-  def updated
+
+  def update
     order = Order.find(params[:id])
     order.update(status: order.status)
     redirect_to admin_order_path(order)
   end
-  
+
   private
   def order_status
     params.require(:order).permit(:status)

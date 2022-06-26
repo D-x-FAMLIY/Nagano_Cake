374d8615762b97cc137e39d5f1279b80e785d6db
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-26 04:11:25 +0000

    修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
   validates :post_code, presence: true
   validates :address, presence: true
   validates :name, presence: true
-  
+
   def address_display
     '〒' + post_code + ' ' + address + ' ' + name
   end
